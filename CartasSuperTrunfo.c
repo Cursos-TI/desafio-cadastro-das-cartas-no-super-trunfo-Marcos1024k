7a1cfb75ee6066ac3a822b86409b8a36353d0fea
Marcos1024k <marcos1024k@gmail.com>
2025-07-04 20:47:40 +0000

    Versao 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 typedef struct {
-    char codigoCarta[3];
+    char codigoCarta[4]; 
     int populacao;
     float area;
     float pib;
@@ -11,8 +11,8 @@ typedef struct {
 int main() {
     Carta carta1, carta2;
 
-   
-    printf("Digite o código da carta 1: ");
+    // Leitura dos dados da Carta 1
+    printf("Digite o código da carta 1 (max 3 caracteres): ");
     scanf("%s", carta1.codigoCarta);
     printf("Digite a população da cidade: ");
     scanf("%d", &carta1.populacao);
@@ -23,8 +23,8 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &carta1.numPontosTuristicos);
 
-   
-    printf("\nDigite o código da carta 2: ");
+    // Leitura dos dados da Carta 2
+    printf("\nDigite o código da carta 2 (max 3 caracteres): ");
     scanf("%s", carta2.codigoCarta);
     printf("Digite a população da cidade: ");
     scanf("%d", &carta2.populacao);
@@ -35,11 +35,12 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &carta2.numPontosTuristicos);
 
-   
+    // Impressão dos dados da Carta 1
     printf("\nDados da Carta 1:\n");
     printf("Código: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n",
            carta1.codigoCarta, carta1.populacao, carta1.area, carta1.pib, carta1.numPontosTuristicos);
 
+    // Impressão dos dados da Carta 2
     printf("\nDados da Carta 2:\n");
     printf("Código: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n",
            carta2.codigoCarta, carta2.populacao, carta2.area, carta2.pib, carta2.numPontosTuristicos);
